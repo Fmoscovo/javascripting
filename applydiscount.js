dc70e48677f152fa59fee5a848d8e113954e8bec
Filipe Moscovo <your@email.com>
2023-06-30 21:31:27 -0600

    random

---
+++ applydiscount.js
@@ -0,0 +1,11 @@
+function applySeniorDiscount(age) {
+  if (age >= 65) {
+    return "You get 20% off!";
+
+  } else {
+    return "You are not eligible for a discount";
+  }
+};
+
+const discount = applySeniorDiscount(66);
+console.log(discount);
---
+++ childdiscount.js
@@ -0,0 +1,9 @@
+function applyChildDiscount(age) {
+  if ( age <= 10) {
+    return "You get 50% off!";
+  } else {
+    return " Sorry. No discount for you!";
+  }
+};  
+
+applyChildDiscount(10);
