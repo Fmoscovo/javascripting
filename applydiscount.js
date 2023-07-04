c3abf7e9b4fbcb1b0a4a224706ddf818433dd5f2
Filipe Moscovo <your@email.com>
2023-07-04 11:00:12 -0600

    Update applydiscount.js

--- applydiscount.js
+++ applydiscount.js
@@ -1,11 +1,10 @@
-function applySeniorDiscount(age) {
+function applySeniorDiscount(age) { 
   if (age >= 65) {
     return "You get 20% off!";
-
   } else {
     return "You are not eligible for a discount";
   }
-};
+}
 
 const discount = applySeniorDiscount(66);
 console.log(discount);
