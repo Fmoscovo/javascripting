6c4e9993067552baddf7030292103df7af4706b8
Filipe Moscovo <your@email.com>
2023-07-04 14:48:49 -0600

    asd

---
+++ SimpleFunctions.js
@@ -0,0 +1,19 @@
+const printtoconsole = (message) => {
+  console.log(message);
+};
+
+printtoconsole("Hello World");
+///////////////////////////////////////////////////
+////
+const somearray = [];
+const addtoarray = (item) => {
+  somearray.push(item);
+};
+addtoarray("Hello World2");
+addtoarray("Hello World3");
+addtoarray("Hello World4");
+addtoarray("Hello World5");
+
+console.log(somearray);
+
+const array23 = ["Hello", "World", "23"];
---
+++ settimeouttest.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+setTimeout(() => { console.log("Hello World2");
+ }, 0);
+console.log("Hello World3");
+
+setInterval(() => { // This will run every 5 seconds
+  
+}, interval);
+
+setTimeout(() => { // This will run once after 5 seconds 
+
+}, interval);
+
